e25abd9a736d4a66a8b23416bd1f07c1357e9b09
Ralph Schülke <ralph.schuelke@justiz.thueringen.de>
2025-03-13 18:07:43 +0100

    fix indents

--- Tests/csharp.tests.ps1
+++ Tests/csharp.tests.ps1
@@ -1,11 +1,12 @@
 BeforeAll {}
 
 Describe "CS files" -Fixture {
-    [system.io.directoryinfo] $testHome = $psscriptroot
+    [system.io.directoryinfo] $testHome = "$psscriptroot/.."
     [system.io.directoryinfo] $modHome = $testHome.Parent
     [string]$ModName = $modHome.Name
-
- if($env:APPVEYOR_JOB_ID)
+    Write-Warning "mod home = $($modhome.fullname)"
+    #Write-Warning
+  if($env:APPVEYOR_JOB_ID)
     {
         $modName = $modName -replace [Regex]::Escape(('-'+$Env:AppVeyor_Job_Id)), ''
     }
@@ -20,42 +21,42 @@ Describe "CS files" -Fixture {
 
     foreach($file in $csSourceFiles)
     {
-	$ns = select-string -pattern '^\s*namespace\s+(.*)$' -Path $file.fullname
+    $ns = select-string -pattern '^\s*namespace\s+(.*)$' -Path $file.fullname
 
-	[microsoft.powershell.commands.matchinfo[]] $classIndicator = Select-String '^\s*[^/].+\b(interface|class|enum)\s+(?<className>[^ :]+):?.*$'  -Path $file.FullName
+    [microsoft.powershell.commands.matchinfo[]] $classIndicator = Select-String '^\s*[^/].+\b(interface|class|enum)\s+(?<className>[^ :]+):?.*$'  -Path $file.FullName
 
-#	write-warning "for element = $($file.name)"
-#	write-warning "testcase len = $($testcases.count)"
-#	write-warning "class indicator len = $($classindicator.matches.count)"
-#	write-warning "group count = $($classindicator.matches.groups.count)"
+    #	write-warning "for element = $($file.name)"
+    #	write-warning "testcase len = $($testcases.count)"
+    #	write-warning "class indicator len = $($classindicator.matches.count)"
+    #	write-warning "group count = $($classindicator.matches.groups.count)"
 
-	if($classindicator.matches.count -eq 0)
-	{
-	    #todo: what do we do with enums?
-	    # ignore those for the moment, but obviously at this point if we have ANY cs file that doesn't have a class definition in it, we're skipping it here.
-	    # Not that great.
+    if($classindicator.matches.count -eq 0)
+    {
+      #todo: what do we do with enums?
+      # ignore those for the moment, but obviously at this point if we have ANY cs file that doesn't have a class definition in it, we're skipping it here.
+      # Not that great.
 	    
-	    write-warning "SKIP: $($file.name)"
-	    continue
-	}
+      write-warning "SKIP: $($file.name)"
+      continue
+    }
 
-	$testCases.Add(@{
-			   ModName = $modName
-			   CsPath = $file.FullName
-			   CsName = $File.BaseName
-			   Namespace = $ns.Line
-			   ClassName = $classIndicator.Matches[0].Groups[2].Value
-		       })
+    $testCases.Add(@{
+         ModName = $modName
+         CsPath = $file.FullName
+         CsName = $File.BaseName
+         Namespace = $ns.Line
+         ClassName = $classIndicator.Matches[0].Groups[2].Value
+           })
     }
 
     Context "namespace" -Fixture {
-	It "<CsName> has a JPMF namespace" -TestCases $testCases -Test {
-	    # Note: There's a few CS files such as interfaces that might not adhere to backend/cmdlet namespaces
-	    # See how to deal with those; do we force them to adhere or do we permit additional namespaces?
-	    $CsPath | Should -Filecontentmatch "^\s*namespace\s+JPMF\.$($ModName)\.(Backend|Cmdlet|API|Exceptions?)\b" -Because $Namespace
-	}
-	It "<CsName> matches class (<ClassName>)" -TestCases $TestCases -Test {
-	    $className | Should -Be $CsName
- 	}
+    It "<CsName> has a JPMF namespace" -TestCases $testCases -Test {
+      # Note: There's a few CS files such as interfaces that might not adhere to backend/cmdlet namespaces
+      # See how to deal with those; do we force them to adhere or do we permit additional namespaces?
+      $CsPath | Should -Filecontentmatch "^\s*namespace\s+JPMF\.$($ModName)\.(Backend|Cmdlet|API|Exceptions?)\b" -Because $Namespace
+    }
+    It "<CsName> matches class (<ClassName>)" -TestCases $TestCases -Test {
+      $className | Should -Be $CsName
+    }
     }
 }
--- get-assemblyrefs.ps1
+++ get-assemblyrefs.ps1
@@ -1,17 +1,25 @@
 [cmdletbinding()]
 Param
 (
-    [parameter(mandatory,valuefrompipeline)]
-    [system.io.fileinfo]$InputObject,
+  [parameter(mandatory,valuefrompipeline)]
+  [system.io.fileinfo]$InputObject,
 
-    [parameter()]
-    [validatenotnullorempty()]
-    [string]$ProjectName = $Env:APPVEYOR_PROJECT_NAME
+  [parameter()]
+  [validatenotnullorempty()]
+  [string]$ProjectName = $Env:APPVEYOR_PROJECT_NAME
 )
 
 process
 {
-    $safeParam = [regex]::escape($ProjectName)
-
-    get-content $inputObject.FullName| where-object {$_ -i<match "^JPMF\.$($safaParam)\."} 
+  $safeParam = [regex]::escape($ProjectName)
+  Write-Warning "Filter: $("JPMF\.$($safeparam)") ($($InputObject.FullName))"
+  $haveNsRefs = Select-String -Path $inputObject.FullName -Pattern "(namespace\s+|\[)JPMF\.$($safeParam)\."
+  if($haveNsRefs)
+  { 
+    New-Object psobject -Property @{
+      HaveNsRefs = $true
+      Path = $InputObject.FullName
+    }
+    Write-Warning "Found a match; updating assembly refs"        
+  }
 }
