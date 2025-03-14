10d2ba0ce18c8ffca80ab4903f4885ac80c70b26
Ralph Schuelke <ralph.schuelke@justiz.thueringen.de>
2025-03-14 04:09:17 +0100

    Add simple assembly build script.

---
+++ build-assembly.ps1
@@ -0,0 +1,27 @@
+using namespace system.io
+[cmdletbinding()]
+Param
+(
+    [parameter()]
+    [validateset('Backend','Cmdlet')]
+    [string]$ProjectType = 'Backend',
+    [parameter()]
+    [directoryinfo]$ProjectRoot = "$PSScriptRoot/.."
+)
+
+begin
+{
+    $errorActionPreference = 'Stop'
+}
+
+process
+{
+    dotnet restore "$(ProjectRoot.FullName)/Source/$($ProjectType)/"
+    dotnet build "$(ProjectRoot.FullName)/Source/$($ProjectType)/"
+    dotnet pack "$(ProjectRoot.FullName)/Source/$($ProjectType)/"
+
+    foreach($package in Get-childitem -recurse -force -filter "JPMF.$(ProjectRoot.Name).$ProjectType.*nupkg")
+    {
+	publish-psresource -repositorye $projectRoot.Name -Path $package.fullname
+    }
+}
