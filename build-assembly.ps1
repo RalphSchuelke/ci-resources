31f790239fd762023a403b04022703889c5fa5a3
Ralph Schuelke <ralph.schuelke@justiz.thueringen.de>
2025-03-14 04:14:27 +0100

    Fix build-assy script

--- build-assembly.ps1
+++ build-assembly.ps1
@@ -16,9 +16,9 @@ begin
 
 process
 {
-    dotnet restore "$(ProjectRoot.FullName)/Source/$($ProjectType)/"
-    dotnet build "$(ProjectRoot.FullName)/Source/$($ProjectType)/"
-    dotnet pack "$(ProjectRoot.FullName)/Source/$($ProjectType)/"
+    dotnet restore "$($ProjectRoot.FullName)/Source/$($ProjectType)/"
+    dotnet build   "$($ProjectRoot.FullName)/Source/$($ProjectType)/"
+    dotnet pack    "$($ProjectRoot.FullName)/Source/$($ProjectType)/"
 
     foreach($package in Get-childitem -recurse -force -filter "JPMF.$(ProjectRoot.Name).$ProjectType.*nupkg")
     {
