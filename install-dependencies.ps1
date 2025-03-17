805b76bae9b80b5b693b01ab77a17ae10d47a6b7
Ralph Schülke <ralph.schuelke@justiz.thueringen.de>
2025-03-17 16:59:47 +0100

    Unroll dependency installation so that we can require specific versions per dependency.

--- install-dependencies.ps1
+++ install-dependencies.ps1
@@ -1,11 +1,16 @@
 [cmdletbinding()]
 Param
 (
-    [string[]] $ModuleList = @('Pester', 'Microsoft.PowerShell.PSResourceGet', 'ModuleBuilder')
+    [hashtable[]] $ModuleList = @(
+    @{ Name = 'Pester';                             Version = '5.7.1'},
+    @{ Name = 'Microsoft.PowerShell.PSResourceGet'; Version = '1.1.0'},
+    @{ Name = 'ModuleBuilder';                      Version = '3.1.7'}
+)
 )
 
 begin
-{
+{   
+    $VerbosePreference = 'Continue'
     $errorActionPreference = 'Stop'
     if($null -eq ( Get-Command dotnet ))
     {
@@ -17,8 +22,17 @@ process
 {
     enable-proxy
     dotnet tool restore --tool-manifest ci/.config/dotnet-tools.json
-    Install-PsResource $ModuleList -Scope CurrentUser
-    Update-PsResource $ModuleList -Scope CurrentUser
-    disable-proxy
-    Import-module -Name $ModuleList
+    foreach($tool in $ModuleList)
+    {
+    # *-PsResource takes a -Version string that is interpreted according to nuget version range definitions,
+    # so there is no -minimumversion/-requiredversion/-maximiumversion.
+    # Passing an explicit version such as '1.2.3' will fetch, or update to, that version if none exists (install)
+    # or an older version is present (update).
+    # -Module in turn uses the classical minimum/maximum/requiredversion specifiers.
+    
+      Install-PsResource @tool -Scope CurrentUser -verbose
+      Update-PsResource @tool -Scope CurrentUser -Verbose
+      Import-Module -Name $tool.Name -RequiredVersion $Tool.Version -Verbose
+
+    }
 }
