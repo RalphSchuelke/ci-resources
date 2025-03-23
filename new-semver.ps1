345196efe931b843fbc147a847d299a09b3bac4e
Ralph Schuelke <ralph.schuelke@justiz.thueringen.de>
2025-03-23 14:58:01 +0100

    debug tfm availability

--- new-semver.ps1
+++ new-semver.ps1
@@ -11,9 +11,10 @@ $errorActionPreference = 'Stop'
 
     # As of Mar 22, 2025 we (still) need to reference net8 8.0.13
     # for gitversion.tool to run.
-    ci/dotnet-install.ps1 -Version 8.0.13 -Runtime dotnet
+    ci/dotnet-install.ps1 -Version 8.0.13 -Runtime windowsdesktop
 }
 process
 {
+    get-command dotnet
  dotnet gitversion /output buildserver /nonormalize /b $BranchName
 }
