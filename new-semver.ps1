28e243bafad3f4defaeab9cf89706f9fccb39178
Ralph Schuelke <ralph.schuelke@justiz.thueringen.de>
2025-03-23 15:24:31 +0100

    Implement pass-able dotnet root

--- new-semver.ps1
+++ new-semver.ps1
@@ -2,7 +2,10 @@
 Param
 (
     [parameter(mandatory)]
-    [string]$BranchName
+    [string]$BranchName,
+
+    [parameter()]
+    [string]$DotNetRoot = ''
 )
 begin
 {
@@ -15,6 +18,22 @@ $errorActionPreference = 'Stop'
 }
 process
 {
+    $useDotnetRoot = if($DotnetRoot.Length -eq 0)
+    {
+	"$(${Env:LocalAppData})/Microsoft/dotnet"
+    }
+    else
+    {
+	$DotnetRoot
+    }
+    If((Test-Path $UseDotnetRoot) -eq $false)
+    {
+	throw new-object argumentexception "DOTNET_ROOT: $($useDotnetRoot): No such path."
+    }
+
+    $Env:Path = $UseDotnetRoot + [System.IO.Path]::PathSeparator + $Env:Path
+    $Env:DOTNET_ROOT = $UseDotnetRoot
+    
     get-command dotnet
     dotnet --info
  dotnet gitversion /output buildserver /nonormalize /b $BranchName
